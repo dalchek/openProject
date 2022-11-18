aac31c03ba917c3b8b00b0edbc39af9c33e4bbe8
dalchek <dalchek999@gmail.com>
2022-11-18 17:16:30 +0100

    git is so nice tooo

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const div = document.createElement("div");
 div.className = "test-container";
 div.classList.add("anotherclass");
-div.style.background = "red";
-div.classList.remove("anotherclass");
+div.style.background = "lightblue";
+div.style.height = "100px";
+div.style.border = "1px solid blue";
+document.body.appendChild(div);
+div.innerHTML = `<div class="card">
+                    <h2>Title</h2>
+                    <p>Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod
+                </div>`;
 console.log(div.outerHTML);
+let conteiner = document.querySelector(".conteiner");
+conteiner.style.backgroundColor = "red";
+conteiner.style.color = "white";
